c12c91ea77bc50fea7e20be22e06b10e2ad07bfc
Dwayne <deandraper82@gmail.com>
2020-11-18 21:09:43 -0600

    Tic Tac Toe complete except reset button.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -2,7 +2,7 @@
   Win Conditions
 ************************************************* */
 
-let winningCombos = [[0,1,2],[0,3,5],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]];
+let winningCombos = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]];
 
 
 /* *************************************************
@@ -12,32 +12,30 @@ MODEL
 let currentPlayer = 'X';
 const playerX = [];
 const playerO = [];
-const board = [];
+const board = new Array(9);
 
 /* *************************************************
 CONTROLLER
 ************************************************* */
 
-const updateSquare = function(e) {
-  if (currentPlayer === 'X') {
-    e.target.innerText = 'X'
-  } else {
-    e.target.innerText = 'O'
-  }
-};
-
 const updatePlayerArray = function(e) {
   if (currentPlayer === 'X') {
-    playerX.push(e.target.id)
+    playerX.push(parseInt(e.target.id));
+    console.log('X', playerX);
   } else {
-    playerO.push(e.target.id)
+    playerO.push(parseInt(e.target.id));
+    console.log('O', playerO);
   }
 }
 
 const isWinner = function(currentPlayer) {
   return winningCombos.some((win) => {
-    return win.every((letter) => {
-      return currentPlayer.includes(letter)
+    return win.every((number) => {
+      if (currentPlayer === 'X') {
+        return playerX.includes(number);
+      } else {
+        return playerO.includes(number);
+      }
     })
   })
 }
@@ -50,32 +48,51 @@ const togglePlayer = function() {
   }
 }
 
-const handleClick = function(e) {
-  updateSquare(e);
-  updatePlayerArray(e);
-  togglePlayer();
+const addMoveToBoard = function(e) {
+  board[e.target.id] = currentPlayer;
 }
 
 /* *************************************************
 VIEW
 ************************************************* */
 
+const updateSquare = function(e) {
+  if (currentPlayer === 'X') {
+    e.target.innerText = 'X'
+  } else {
+    e.target.innerText = 'O'
+  }
+};
+
+// const updateView = function(winner) {
+//   if (winner) {
+//     document.getElementById()
+//   }
+// }
+
 /* *************************************************
   MAIN
 ************************************************* */
 
+document.getElementById('gamestatus').innerText = 'It is Player X\'s turn';
+
 document.querySelectorAll('#myTable td').forEach((e) => e.addEventListener('click', function(e) {
-  updateSquare(e);
-  updatePlayerArray(e);
-  if (isWinner(currentPlayer)) {
-    document.getElementById('gamestatus').innertext('Congratulations. Player' + currentPlayer + ' won the game!!')
+  if (board[e.target.id] === undefined) {
+    updateSquare(e);
+    addMoveToBoard(e);
+    updatePlayerArray(e);
+    if (isWinner(currentPlayer)) {
+      document.getElementById('gamestatus').innerText = 'Congratulations. Player' + currentPlayer + ' won the game!!';
+    } else {
+      togglePlayer();
+      document.getElementById('gamestatus').innerText = 'It is Player ' + currentPlayer + '\'s turn.';
+    }
   } else {
-    togglePlayer();
-    document.getElementById('gamestatus').innertext('It is Player ' + currentPlayer + '\'s turn.')
+    document.getElementById('gamestatus').innerText = 'It is Player' + currentPlayer + '\'s turn.';
   }
 }))
 
-
+//document.getElementById('gamestatus').innerText =
 
 // let array = [1, 2, 3];
 // undefined
