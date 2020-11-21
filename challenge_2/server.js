c9fcf609bbedacd3a04c93ccb8cd16654e2dabdf
Dwayne Draper <deandraper82@gmail.com>
2020-11-20 20:14:22 -0600

    This is it

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
 
+app.use(express.static('client'))
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
 
+app.post('/upload_JSON', (req, res) => {
+  console.log(req.body.text);
+  res.sendStatus(201);
+});
+
+app.use('/', router)
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}.`)
+});
