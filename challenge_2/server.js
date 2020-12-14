d4a089b1455b2f54aa8de79696852c2e6aaefd0a
Dwayne Draper <deandraper82@gmail.com>
2020-12-14 07:17:56 -0600

    Pushing challenge 2

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,28 +1,40 @@
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
 const utils = require('./utils/utils.js');
+const fs = require('fs');
+const path = require('path');
 const multer = require('multer');
-const upload = multer({dest:'uploads/'})
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+})
+const upload = multer({storage: storage})
 
 app.use(express.static('client'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
-app.post('/', (req, res) => {
-  let json = JSON.parse(req.body.submitData);
-  let csv = utils.convertToCSV(json);
-  res.send(csv);
-});
-
-// app.post('/', upload.single('json'), (req, res) => {
-//   try {
-//     res.send(req.file);
-//   } catch(err) {
-//     res.send(400);
-//   }
+// app.post('/', (req, res) => {
+//   let json = JSON.parse(req.body.submitData);
+//   let csv = utils.convertToCSV(json);
+//   res.send(csv);
 // });
+
+app.post('/', upload.single('jsonFile'), (req, res) => {
+
+  try {
+    res.send(req.file);
+  } catch(err) {
+    res.send(400);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}.`)
