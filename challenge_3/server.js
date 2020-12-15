67318dc41cb7ed49205b6d914387459571b69f92
Dwayne Draper <deandraper82@gmail.com>
2020-12-14 20:38:20 -0600

    Switch

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+express.json();
+express.urlencoded();
 
+app.use(express.static('/public'));
+
+app.listen(port, () => {
+  console.log('Checkout app listening at http://localhost:${port}');
+})
 
