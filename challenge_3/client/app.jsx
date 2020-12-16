e1c651a6a476f3f7eba804ec3d871664fe4580ec
Dwayne Draper <deandraper82@gmail.com>
2020-12-15 19:47:00 -0600

    Switching computers

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -2,13 +2,20 @@
 // import ReactDOM from 'react-dom';
 
 function Checkout(props) {
-  return  <h1>Testing that this works</h1>
+  return  <h1>This is checkout</h1>
 }
-// Account
-// Shipping
-// CreditCard
-// Something else
-//Something else
+function Account(props) {
+  return  <h1>This is account creation</h1>
+}
+function Shipping(props) {
+  return  <h1>This is shipping details</h1>
+}
+function CreditCard(props) {
+  return  <h1>This is where I take your damn money!</h1>
+}
+function Summary(props) {
+  return  <h1>This is where the summary is</h1>
+}
 
 
 class App extends React.Component {
@@ -16,19 +23,36 @@ class App extends React.Component {
     super(props)
     this.state = {
       currView: 'Checkout',
-      userName: ''
+      username: ''
     }
+    this.handleOnClick = this.handleOnClick.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
-  handleOnClick
-  (newView) {
+  handleOnClick(newView) {
     this.setState({
       currView: newView
     })
   }
+
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleSubmit(e) {
+  }
+
   render() {
     return (
       <div>
-        <Checkout />
+        <form onSubmit={this.handleSubmit}>
+          <input type="username"
+          name="username"
+          value={this.state.value}
+          onChange={this.handleChange}>
+
+          </input>
+        </form>
         Some content will go here.
       </div>
     )
