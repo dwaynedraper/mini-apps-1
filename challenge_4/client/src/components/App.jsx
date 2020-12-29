3440d6a348791b42b076d34eefb7d46218ff2c31
Dwayne Draper <deandraper82@gmail.com>
2020-12-29 12:01:15 -0600

    Pushing end of session. Idea refactored to keep all state in App. Squares will use conditional rendering

--- challenge_4/client/src/components/App.jsx
+++ challenge_4/client/src/components/App.jsx
@@ -1,6 +1,31 @@
+import React from 'react';
+import Row from './Row';
+
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      currentPlayer: null,
+      board = [],
+      isWinner = false,
+      message = '',
+      errorMessage = ''
+    }
+  }
+
+  initializeBoard() {
+    //do stuff
+  }
+  updateSquare() {
+    //do stuff
+  }
+  render() {
+    let rows = this.board.map(//createRows)
+    return (
+      <div className="container" >
+        {this.rows}
+      </div>
+    )
   }
 }
 
---
+++ challenge_4/client/src/components/Row.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Square from './Square';
+
+const Row = () => {
+  return (
+    <div>
+      // Generate 7 squares and assign them proper keys and props
+    </div>
+  )
+}
+
+export default Row;
