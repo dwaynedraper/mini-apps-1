1dd387c23eadbb13a9a91a799c29f88040d7047d
Dwayne Draper <deandraper82@gmail.com>
2021-01-07 07:14:33 -0600

    Cleaned up stray code. Assignment Complete

--- challenge_4/client/src/components/App.jsx
+++ challenge_4/client/src/components/App.jsx
@@ -33,9 +33,6 @@ class App extends React.Component {
   }
 
   updateSquare(col) {
-    //do stuff
-    //can't set state directly. Variable to use to update board.
-    // console.log('updateSquare method fired', col)
     if (!this.state.isWinner) {
       let board = this.state.board
       let position = [null, col];
@@ -44,10 +41,10 @@ class App extends React.Component {
           if (board[row][col] === 0) {
             board[row][col] = this.state.currentPlayer;
             position[0] = row
-            break; //was filling the entire col by accident - this prevents
+            break;
           }
         }
-      }//if
+      }
 
       if (this.checkForWinner(position)) {
         this.setState({isWinner: true, message: `Player ${this.state.currentPlayer} won the game!!`})
@@ -60,10 +57,7 @@ class App extends React.Component {
   }
 
   checkForWinner(position) {
-    console.log('beginning this turn\'s check for winner')
-    console.log('---------------------------------------')
     let currentPlayer = this.state.currentPlayer;
-    // console.log(currentPlayer, 'curr')
     if (this.checkVertical(position, currentPlayer)) {return true}
     if (this.checkHorizontal(position, currentPlayer)) {return true}
     if (this.checkBackDiagonal(position, currentPlayer)) {return true}
@@ -76,17 +70,13 @@ class App extends React.Component {
     let row = position[0];
     let col = position[1];
     let orig = [row, col];
-    // console.log('Initial value', row, col)
     let board = this.state.board;
-    // console.log('board during horizontal check', board)
     if (row > 0) {
       do {
         col--
-        // console.log('Value after --', row, col)
         if (board[row][col] === currentPlayer) {
           count ++
           if (count === 4) {
-            // console.log('Oh look, horizontal win!')
             return true;
           }
         } else {
@@ -98,13 +88,10 @@ class App extends React.Component {
     }
     if (row < 6) {
       do {
-        // console.log('Initial value', row, col)
         col++
-        // console.log('Value after ++', row, col)
         if (board[row][col] === currentPlayer) {
           count ++
           if (count === 4) {
-            // console.log('Oh look, horizontal win!')
             return true;
           }
         } else {
@@ -121,14 +108,10 @@ class App extends React.Component {
     let board = this.state.board;
     if (row < 3) {
       do {
-        // console.log('initial row value', row, col);
         row++;
-        // console.log('value after ++', row, col)
-        // console.log('board that do loop is running:', board)
         if (board[row][col] === currentPlayer) {
           count++
           if (count === 4) {
-            // console.log('Oh look, vertical winner!!')
             return true;
           }
         } else {
@@ -146,13 +129,10 @@ class App extends React.Component {
     let row = position[0];
     let col = position[1];
     let orig = [row, col];
-    console.log('Initial value', row, col)
     let board = this.state.board;
-    console.log('board during diagonal check', board)
     while (row > 0 && col > 0) {
       row--;
       col--;
-      // console.log('updated value to check', row, col);
       if (board[row][col] === currentPlayer) {
         count++;
         if (count === 4) return true;
@@ -165,7 +145,6 @@ class App extends React.Component {
     while (row < 5 && col < 6) {
       row++;
       col++;
-      // console.log('updated value to check', row, col);
       if (board[row][col] === currentPlayer) {
         count++;
         if (count === 4) return true;
@@ -174,41 +153,6 @@ class App extends React.Component {
       }
     }
     return false;
-    // if (row > 0 && col > 0) {
-    //   do {
-    //     col--
-    //     row--
-    //     console.log('Value after reverse', row, col)
-    //     if (board[row][col] === currentPlayer) {
-    //       count ++
-    //       if (count === 4) {
-    //         console.log('Oh look, diagonal win!')
-    //         return true;
-    //       }
-    //     } else {
-    //       row = orig[0];
-    //       col = orig[1];
-    //       break
-    //     }
-    //   } while (row > 0 && col > 0)
-    // }
-    // if (row < 6 && col < 5) {
-    //   do {
-    //     // console.log('Initial value', row, col)
-    //     col++
-    //     row++
-    //     console.log('Value after forward', row, col)
-    //     if (board[row][col] === currentPlayer) {
-    //       count ++
-    //       if (count === 4) {
-    //         console.log('Oh look, diagonal win!')
-    //         return true;
-    //       }
-    //     } else {
-    //       break
-    //     }
-    //   } while (row < 6 && col < 5)
-    // }
   }
 
   checkBackDiagonal(position, currentPlayer){
@@ -217,13 +161,11 @@ class App extends React.Component {
     let row = position[0];
     let col = position[1];
     let orig = [row, col];
-    console.log('Initial value', row, col)
     let board = this.state.board;
-    console.log('board during diagonal check', board)
+
     while (row < 5 && col > 0) {
       row++;
       col--;
-      // console.log('updated value to check', row, col);
       if (board[row][col] === currentPlayer) {
         count++;
         if (count === 4) return true;
@@ -236,7 +178,6 @@ class App extends React.Component {
     while (row > 0 && col < 6) {
       row--;
       col++;
-      // console.log('updated value to check', row, col);
       if (board[row][col] === currentPlayer) {
         count++;
         if (count === 4) return true;
